b2cd63ef33bfb849591c65e473496d4b1b4d2ef0
yang8629 <8629yang@gmail.com>
2018-01-16 21:20:16 +0800

    Add files via upload

--- js/jquery.js
+++ js/jquery.js
@@ -12,4 +12,19 @@ $(document).ready(function(){
 		$('.ticket').hide();
 		$($(this).attr('href')).show();
 	});
+
+	$(".flip--1").click(function(){
+    $(".panel--1").slideToggle();
+		$(this).toggleClass("flip--background");
+  });
+
+	$(".flip--2").click(function(){
+    $(".panel--2").slideToggle("fast");
+		$(this).toggleClass("flip--background");
+  });
+
+	$(".flip--3").click(function(){
+    $(".panel--3").slideToggle();
+		$(this).toggleClass("flip--background");
+  });
 });
